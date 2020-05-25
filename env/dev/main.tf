e4d4453157d766cb8477d8251f07bbc7f7c78052
kalyanim7 <60805003+kalyanim7@users.noreply.github.com>
2020-05-24 19:59:56 -0500

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "infracreate"
-    key    = "k8smod/terraform.tfstate"
+    key    = "ansiblemod/terraform.tfstate"
     region = "us-east-2"
   }
 }
